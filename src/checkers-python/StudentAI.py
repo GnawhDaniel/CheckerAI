6fa36e14c143a60be00124a82d04e98428feeee8
Daniel Hwang <danielhwang02@gmail.com>
2022-11-01 16:45:28 -0700

    Started selection, expand, and get_move functions.

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -3,7 +3,23 @@ from BoardClasses import Move
 from BoardClasses import Board
 #The following part should be completed by students.
 #Students can modify anything except the class name and exisiting functions and varibles.
+import math
+import copy
+
+C = 2
+
+class Node():
+    def __init__(self, board, s_p):
+        self.board = copy.deepcopy(board)
+        self.s_i = 0
+        self.w_i = 0 
+        self.s_p = s_p
+        self.children = []
+    
 class StudentAI():
+    @staticmethod
+    def uct_score(w_i, s_i, s_p):
+        return (w_i/s_i) + C*(math.sqrt(math.log(s_p)/s_i))
 
     def __init__(self,col,row,p):
         self.col = col
@@ -14,14 +30,51 @@ class StudentAI():
         self.color = ''
         self.opponent = {1:2,2:1}
         self.color = 2
+
+        self.root = Node(self.board, 0)
+
     def get_move(self,move):
         if len(move) != 0:
             self.board.make_move(move,self.opponent[self.color])
         else:
             self.color = 1
+
         moves = self.board.get_all_possible_moves(self.color)
-        index = randint(0,len(moves)-1)
-        inner_index =  randint(0,len(moves[index])-1)
-        move = moves[index][inner_index]
+
+
         self.board.make_move(move,self.color)
         return move
+
+    def selection(self):
+        root = self.root
+
+        while root.children != []:
+            best_uct_value = float('-inf')
+            best_uct_node = None
+            for child in root.children:
+                uct_value = StudentAI.uct_score(child.w_i, child.s_i, child.s_p)
+                if uct_value > best_uct_value:
+                    best_uct_value = uct_value
+                    best_uct_node = child
+
+            if best_uct_node is not None:
+                root = best_uct_node
+            else:
+                self.expand(root)
+                # 1. Run simulation
+                # 2. Update root values
+                break
+
+        return # Move 
+    
+    def expand(self, root):
+        moves = root.board.get_all_possible_moves(self.color)
+        
+
+    def simulation(self):
+        pass
+                
+        
+
+if __name__ == "__main__":
+    pass
