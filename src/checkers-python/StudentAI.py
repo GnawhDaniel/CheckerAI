8de02471db3a4f9eaa516b9c327689db0c2c3325
Daniel Hwang <danielhwang02@gmail.com>
2022-11-02 01:42:17 -0700

    update

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -1,4 +1,5 @@
 from random import randint
+from tracemalloc import start
 from BoardClasses import Move
 from BoardClasses import Board
 #The following part should be completed by students.
@@ -7,20 +8,29 @@ import math
 import copy
 
 C = 2
+ITERATIONS = 3
 
 class Node():
-    def __init__(self, board, s_p):
+    def __init__(self, board, color, previous, move=None):
         self.board = copy.deepcopy(board)
+        self.move = move
+        self.color = color
+        self.previous = previous
+
         self.s_i = 0
         self.w_i = 0 
-        self.s_p = s_p
         self.children = []
     
+    def recalculate(self):
+        w_i = 0
+        s_i = 0
+        for child in self.children:
+            w_i += child.s_i - child.w_i
+            s_i += child.s_i
+        self.w_i = w_i
+        self.s_i = s_i
+    
 class StudentAI():
-    @staticmethod
-    def uct_score(w_i, s_i, s_p):
-        return (w_i/s_i) + C*(math.sqrt(math.log(s_p)/s_i))
-
     def __init__(self,col,row,p):
         self.col = col
         self.row = row
@@ -30,8 +40,12 @@ class StudentAI():
         self.color = ''
         self.opponent = {1:2,2:1}
         self.color = 2
+        
+        # My code
+        self.root = None
 
-        self.root = Node(self.board, 0)
+    def uct_score(self, node):
+        return (node.w_i/node.s_i) + C*(math.sqrt(math.log(node.previous.s_i)/node.s_i))
 
     def get_move(self,move):
         if len(move) != 0:
@@ -40,41 +54,71 @@ class StudentAI():
             self.color = 1
 
         moves = self.board.get_all_possible_moves(self.color)
+        self.root = Node(self.board, self.color, None)
 
+        i = 0
+        while (i < ITERATIONS):
+            self.selection(self.root)
+            i += 1
+        
+        curr_max_val = float('-inf') 
+        best_move = None
+        for node in self.root.children:
+            if node.s_i > curr_max_val:
+                curr_max_val = node.s_i
+                best_move = node.move
+        print(self.board.is_valid_move(best_move))
 
-        self.board.make_move(move,self.color)
+        self.board.make_move(best_move, self.color)
         return move
 
-    def selection(self):
-        root = self.root
-
-        while root.children != []:
+    def selection(self, root):
+        if root.children == []:
+            self.expand(root)
+            root.recalculate()
+        else:
             best_uct_value = float('-inf')
             best_uct_node = None
             for child in root.children:
-                uct_value = StudentAI.uct_score(child.w_i, child.s_i, child.s_p)
+                uct_value = self.uct_score(child)
                 if uct_value > best_uct_value:
-                    best_uct_value = uct_value
                     best_uct_node = child
-
-            if best_uct_node is not None:
-                root = best_uct_node
-            else:
-                self.expand(root)
-                # 1. Run simulation
-                # 2. Update root values
-                break
-
-        return # Move 
-    
+            self.selection(best_uct_node)
+            # Recalulate root's variables
+            root.recalculate()
+            
     def expand(self, root):
-        moves = root.board.get_all_possible_moves(self.color)
-        
+        color = 2 if root.color==1 else 1 # FIX ME
+        moves = root.board.get_all_possible_moves(root.color)
 
-    def simulation(self):
-        pass
-                
+        for move1 in moves:
+            for move in move1:
+                # Create New Nodes
+                board = copy.deepcopy(self.board)
+                board.make_move(move, root.color)
+                node = Node(board, color, root, move)
+                root.children.append(node)
+
+                # Simulate
+                winner = self.simulation(root.board, color)
+                if winner != color:
+                    node.w_i += 1
+                node.s_i += 1
         
+    def simulation(self, board, player):
+        board = copy.deepcopy(board)
+
+        while winner:=(board.is_win(player)):
+            
+            moves = board.get_all_possible_moves(player)
+            index = randint(0, len(moves)-1)
+            inner_index = randint(0, len(moves[index])-1)
+            move = moves[index][inner_index]
+            board.make_move(move, player)
+
+            player = (2 if player == 1 else 1) # FIX ME
+
+        return winner
 
 if __name__ == "__main__":
     pass
